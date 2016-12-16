0108da0fc7460f9c3978fb65cca3a7159262cc3e
Ramsey D'silva <ramseydsilva@gmail.com>
2016-12-16 13:55:06 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,79 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+import os
+
+def package_files(directory):
+    paths = []
+    print(directory)
+    for (path, directories, filenames) in os.walk(directory):
+        print(path, directories, filenames)
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+setup(
+    name='docs',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.0.1',
+
+    description='ML package',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://isac.ramseydsilva.com',
+
+    # Author details
+    author='Ramsey D\'silva',
+    author_email='ramseydsilva@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='sample setuptools development',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=['docs'],
+
+    #install_requires=[],
+    #extras_require={},
+    package_data={},
+    include_package_data=True,
+    #data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'server=server:main',
+        ],
+    },
+)
