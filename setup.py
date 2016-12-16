9e785da89f846b2a08d51242f569ef9bf1cace94
Ramsey D'silva <ramseydsilva@gmail.com>
2016-12-16 14:13:03 +0100

    Move files to root

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['docs'],
+    packages=[],
 
     #install_requires=[],
     #extras_require={},
